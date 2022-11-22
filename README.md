30018469e79cb0e2e8ed97eb6fa97c0f36ff0a88
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-11-22 17:06:32 +0100

    Fix all linters

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
   <h3><b>Hello Rails React</b></h3>
 </div>
 
-## Link to FrontEnd : https://github.com/Said-laasri/react-front-end/pull/1
+## Link to BackEnd : https://github.com/Said-laasri/rails_back_end/tree/setup
 
 
 # 📗 Table of Contents
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux';
-import './index.css'
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
