66205af8676784c6f9e8b231bc096ba12939b1ec
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-11-22 16:31:21 +0100

    Add react-dom/client

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+import Greeting from './components/Greeting';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello Friend</h1>
+      <Link to="/greeting">Welcome</Link>
+      <Routes>
+        <Route path="/greeting" element={<Greeting />} />
+      </Routes>
     </div>
-  );
-}
+  )
+};
 
 export default App;
---
+++ src/redux/greetings/greetings.js
@@ -0,0 +1,27 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+const API_URL = 'http://localhost:3000/greetings';
+
+const FETCH = 'hello-react-rails/greetings/FETCH';
+
+const initialState = {
+  greeting: '',
+};
+
+
+const greetingsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case `${FETCH}/fulfilled`:
+      return { ...state, greeting: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const fetchGreeting = createAsyncThunk(FETCH, async () => {
+  const response = await fetch(API_URL);
+  const data = await response.json();
+  return data.message;
+});
+
+export default greetingsReducer;
